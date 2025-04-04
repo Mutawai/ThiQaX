aff8e18626b0de4abaf63a3192b2aafa51f7bdbb
Mutawai <scribemosaic@gmail.com>
2025-04-04 11:33:46 +0300

    Add server entry point

---
+++ src/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
