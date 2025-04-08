78320dab4df1fb562f2104f8a2b32f64b1887dff
Mutawai <scribemosaic@gmail.com>
2025-04-08 21:30:30 +0300

    Create index.js

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+// services/index.js
+export { default as authService } from './authService';
+export { default as adminService } from './adminService';
+export { default as applicationService } from './applicationService';
+export { default as documentIntegrationService } from './documentIntegrationService';
+export { default as documentUploadService } from './documentUploadService';
+export { default as emailService } from './emailService';
+export { default as jobService } from './jobService';
+export { default as kycService } from './kycService';
+export { default as messageService } from './messageService';
+export { default as notificationService } from './notificationService';
+export { default as profileIntegrationService } from './profileIntegrationService';
+export { default as searchService } from './searchService';
