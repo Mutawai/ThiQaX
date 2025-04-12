f5db7366c763cf778cb8071470579fa92c9ef16f
Mutawai <scribemosaic@gmail.com>
2025-04-12 19:30:26 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,44 @@
-// client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ToastContainer } from 'react-toastify';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store';
+import { store, persistor } from './redux/store';
+import theme from './theme';
 
-// Import global styles
-import './assets/css/index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
