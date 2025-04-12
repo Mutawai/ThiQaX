7fab9b818f6fff67840c9c050514123925110765
Mutawai <scribemosaic@gmail.com>
2025-04-12 20:05:08 +0300

    Create babel.config.js

---
+++ client/babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
+};
